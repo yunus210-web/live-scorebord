aacfa97fdb86caf7279f95719e14082f8a291efe
Yunus Ks <yunusakd210@gmail.com>
2026-08-13 14:56:08 +0530

    Check results route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 app.use('/api/results',resultRoutes);
+console.log("RESULT ROUTE LOADED")
 // In-memory scoreboard for now
 let scoreboard = {
   eventName: 'HIM MEELAD FEST',
